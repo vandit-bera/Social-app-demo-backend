cde723db39e4ca9cdbc7c9e19fe1100cecff4bea
vandit-bera <vanditbera@gmail.com>
2023-04-20 22:26:49 +0530

    Create first app in express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Vandit")
+const express = require("express")
+
+const app = express()
+
+const PORT = 4000
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello, Vandit")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`)
+})
