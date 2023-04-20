27735d72a375a2ba5bbffa55e53cafc864b2bcac
vandit-bera <vanditbera@gmail.com>
2023-04-20 22:35:26 +0530

    Create 2 api in get

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ app.get("/", (req, res) => {
     res.status(200).send("Hello, Vandit")
 })
 
+app.get("/api/v1/instagram", (req, res) => {
+    const instagramSocial = {
+        username: "vandit.bera",
+        followers: 200,
+        follows: 206,
+        date: Date.now()
+    }
+
+    res.status(200).json({ instagramSocial })
+})
+
+app.get("/api/v1/linkedin", (req, res) => {
+    const linkedSocial = {
+        username: "vandit_bera",
+        followers: 580,
+        follows: 420,
+        date: Date.now()
+    }
+
+    res.status(200).json( linkedSocial )
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`)
 })
